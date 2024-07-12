5190e3a115bbc82aa356af8c6f0f4fad6b36cf46
adilbarki <168727835+adilbarki@users.noreply.github.com>
2024-07-12 05:07:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+//Quesion-01
+// Write a JavaScript program that asks the user for a string and an index, and then displays the character at that index in the string using the javascript method.
+
+// let a = prompt("Enter String");
+// let index = prompt("Enter index");
+// let charindex = a.charAt(index);
+// alert(`The character at index ${index} is: ${charindex}`);
+// alert("The character at index" + index + "is:" + charindex);
+
+//Quesion-02
+// Write a JavaScript program that asks the user for a string, a substring to search for, and a replacement substring, and then displays the modified string with all occurrences of the search substring replaced using the replace() method.
+let stri = prompt("Enter String");
+// let search = prompt("Enter sub String for searching");
+// let replacement = prompt("Enter a replacement sub String");
+// let modifiedstr = stri.replace(search,replacement)
+// alert(`The Modified String Is ${modifiedstr}`)
+
+//Quesion-03
+// Write a JavaScript program that asks the user for a number and then displays the rounded version of the number to the nearest whole number using the round() method
+
+// let num = +prompt("Enter a number");
+// let rnum = Math.round(num)
+// alert(rnum);
+
+//Quesion-04
+// Write a JavaScript program that asks the user for a number and then displays the ceiling of the number using the ceil() method. For example, if the user inputs 4.2, the program should display 5.
+
+// let num = (prompt("Enter a number"));
+// let rnum = Math.ceil(num);
+// alert(rnum);
+
+// //Quesion-05
+// Write a JavaScript program that asks the user for a number and then displays the flooring of the number using the floor() method. For example, if the user inputs 8.6, the program should display 8.
+// let num = (prompt("Enter a number"));
+// let rnum = Math.floor(num);
+// alert(rnum);
